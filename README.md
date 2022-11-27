a32ea97705a5a3d52844bc32112a2b507cad4378
belajarqywok <belajarqywok@gmail.com>
2022-11-27 23:11:30 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# KopiLoe_infra" 
---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Setup The Repository
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker Engine
+sudo apt-get install \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
+    docker-compose-plugin
+
+# Create Docker Volume
+docker volume create --name=kopiloe_db_vol
+
+# Build App via Docker Compose
+docker-compose up -d 
+
+# Create Docker Swarm
+docker swarm init --advertise-addr $1
